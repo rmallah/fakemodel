66fffed507a3da618da1bc39da986482425e90d4
Rajesh Kumar Mallah <mallah.rajesh@gmail.com>
2017-12-14 15:35:10 +0530

    Initial code commit

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
 # fakemodel
-Generates placeholder models based on specifications
+Generates placeholder QML models based on specifications
+
+
+# usage
+
+```
+usage: ./genmodel.pl --name|-n --model|-m [--help|-h] [--sepcount|-sc]
+[--sep|-s] [--count|-c]
+
+program generates fake QML model for app modelling.
+
+required named arguments:
+  --name, -n NAME        name of model
+  --model, -m MODEL      Model in fieldname:type[:range-min:range-max] format eg:
+                           id:number,name:string,age:number,city:string (simple)
+                           id:number,name:string:,age:number:20:65,city:string:2:6
+                           (full)
+                           
+                           possible types:
+                           
+                           'string' : a short text consiting of 3 to 5 words.
+                             'text' : a chunk of text consiting of 1 to 4 sentences.
+                           'number' : a random number between 0 100
+                           'float'  : a floating point value between 0 100
+                           'serial' : an autoincremting number with each list element.
+                           'epoch'  : an epoch value (unix timestamp) between min to
+                           max days from now . (use negative for timestamps of past)
+                           
+                           range specs:
+                           each type can be optionally followed by min:max that allows
+                           to specify the range of length of text or values of
+                           numbers.
+
+optional named arguments:
+  --help, -h                  ? show this help message and exit
+  --sepcount, -sc SEPCOUNT    ? separator count
+                                  Default: 4
+  --sep, -s SEP               ? indent preference
+                                  Choices: [space, tab], case sensitive
+                                  Default: space
+  --count, -c COUNT           ? howmany elements in model
+                                  Default: 2
+
+Please correct the invocation of command.
+
+```
+
+# example output
+
+`./genmodel.pl --model "student_id:serial,name:string,age:number:18:30,resume:text,generated:epoch:-30:0" --name Students --count 4`
+
+```
+
+import QtQuick 2.0;
+
+Students {
+     ListElement {
+            student_id: 1
+            name: 'porro explicabo perferendis odio'
+            age: 24
+            resume: 'Cumque ipsam voluptatibus eaque.'
+            generated: 1512067712
+    }
+     ListElement {
+            student_id: 2
+            name: 'rerum cum expedita'
+            age: 20
+            resume: 'Ut non deserunt fuga velit excepturi.'
+            generated: 1512284673
+    }
+     ListElement {
+            student_id: 3
+            name: 'provident voluptatum tempore recusandae'
+            age: 28
+            resume: 'Inventore perspiciatis est quisquam.'
+                + 'Beatae necessitatibus vero mollitia a'
+                + 'sint nostrum omnis. Aut et tempora'
+                + 'ducimus delectus porro et soluta.'
+            generated: 1511454873
+    }
+     ListElement {
+            student_id: 4
+            name: 'est porro exercitationem non'
+            age: 18
+            resume: 'Accusantium vitae deserunt magni'
+                + 'soluta.'
+            generated: 1512164838
+    }
+}
+
+```
---
+++ genmodel.pl
@@ -0,0 +1,145 @@
+#!/usr/bin/perl
+
+
+use strict;
+
+use Getopt::ArgParse ;
+use Text::Lorem;
+use Text::Wrap;
+
+use Log::Log4perl qw(:easy);
+Log::Log4perl->easy_init($DEBUG);
+
+
+my $logger = get_logger();
+
+# $logger -> debug ("Program invoked with args: @ARGV");
+
+my $ap = Getopt::ArgParse->new_parser(
+        prog        => "$0" ,
+        description => 'program generates fake QML model for app modelling.',
+        epilog      => 'Please correct the invocation of command.',
+);
+
+
+my $modelhelp = qq{Model in fieldname:type[:range-min:range-max] format eg:
+id:number,name:string,age:number,city:string (simple)
+id:number,name:string:,age:number:20:65,city:string:2:6 (full)
+
+possible types:
+
+'string' : a short text consiting of 3 to 5 words.
+  'text' : a chunk of text consiting of 1 to 4 sentences.
+'number' : a random number between 0 100
+'float'  : a floating point value between 0 100
+'serial' : an autoincremting number with each list element.
+'epoch'  : an epoch value (unix timestamp) between min to max days from now . (use negative for timestamps of past)
+
+range specs:
+each type can be optionally followed by min:max that allows
+to specify the range of length of text or values of numbers.
+};
+
+$ap->add_arg('--model'   , '-m', type=>'Array' , split=> ',' ,  required => 1 , help => $modelhelp );
+$ap->add_arg('--name'    , '-n', type=>'Scalar' ,required => 1 , help => 'name of model' );
+$ap->add_arg('--count'    , '-c', type=>'Scalar' ,default => 2 , help => 'howmany elements in model' );
+$ap->add_arg('--sep'    , '-s', type=>'Scalar' ,default => 'space' , choices=>[ 'space', 'tab'] ,  help => 'indent preference' );
+$ap->add_arg('--sepcount' , '-sc', type=>'Scalar' ,default => 4 , help => 'separator count' );
+
+
+
+my $ns = $ap->parse_args();
+my @model  = $ns->model  ;
+my $name   = $ns->name   ;
+my $count  = $ns->count  ;
+my $sep    = $ns->sep eq 'space' ? " " : "\t"  ;
+my $sepcount  = $ns->sepcount ;
+
+
+my $lorem = Text::Lorem->new();
+$Text::Wrap::columns = 40;
+
+my $model = qq!
+import QtQuick 2.0;
+
+$name {!;
+
+my $seq=1;
+
+for(1..$count) {
+		$model .= &ListElement();
+}
+
+$model .= qq!\n}\n!;
+
+print $model;
+
+
+sub ListElement {
+
+		my ($h) = @_;
+
+		my $spacing = $sep x $sepcount;
+		my $le = qq!\n$spacing ListElement {!;
+		my $rand;
+
+		my $fakevalue ;
+		foreach my $f (@model) {
+				my ($name,$type,$min,$max) = split /\s*:\s*/ , $f;
+
+				$fakevalue ="";
+
+				if ($type eq 'string') {
+					$rand = randNumber(  $min || 3 , $max || 5);
+					$fakevalue = "'" . $lorem -> words($rand) . "'"  ;
+				} elsif ($type eq 'text') {
+
+					$rand = randNumber(  $min || 1 , $max || 4);
+					my $sentances =  $lorem -> sentences($rand) ;
+
+					my @lines = split "\n" , wrap('', '', $sentances );
+
+					my $lines =  join "\n$spacing$spacing$spacing$spacing+ " , map { "'$_'";  }   @lines;
+
+
+					$fakevalue = $lines  ;
+				} elsif ($type eq 'number') {
+					$rand = randNumber(  $min || 0 , $max || 100);
+					$fakevalue = int $rand ; 
+
+				} elsif ($type eq 'serial') {
+
+					$fakevalue = $seq++; 
+
+				} elsif ($type eq 'float') {
+					$rand = randNumber(  $min || 0 , $max || 100);
+					$fakevalue = $rand;
+				} elsif ($type eq 'epoch') {
+					my $now = time ;
+					my $randdays = randNumber($min || -30 , $max || 0);
+					$fakevalue = int( $now + $randdays * 86400);
+				}
+
+				else {
+
+					warn "sorry invalid type specified : $type\n";
+					$ap->print_usage;
+					exit 1;
+
+				}
+
+				$le .= "\n$spacing$spacing$spacing$name: $fakevalue";
+		}
+
+		$le .= qq!\n$spacing}!;
+
+		return $le;
+
+}
+
+sub randNumber {
+	my ($min , $max) = @_;
+	return $min + rand ($max-$min);
+}
+
+
